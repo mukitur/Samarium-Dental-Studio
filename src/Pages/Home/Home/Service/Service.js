df46b63473f737017cd3c7a79bae88bb2af58966
mukitur <mukit.inway@gmail.com>
2021-10-18 13:02:59 +0600

    services section add

---
+++ src/Pages/Home/Home/Service/Service.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button, Card, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Service = ({service}) => {
+    const {img, name, short_description} = service;
+    return (
+        <div>
+            <div>
+                <Col>
+                    <Card>
+                        <Card.Img variant="top" src={img} />
+                        <Card.Body>
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Text>
+                            {short_description}
+                        </Card.Text>
+                        <Link to = '/contact'><Button variant="info">Book for {name}</Button></Link>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </div>
+        </div>
+    );
+};
+
+export default Service;
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,20 +1,38 @@
-import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
+import Service from '../Home/Service/Service';
 
 const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect( () => {
+        fetch('./fakeData.json')
+            .then(res=> res.json())
+            .then(data=>setServices(data))
+    },[])
     return (
         <div className="my-5">
-            <Container>
-            <Row>
-                
-                <Col xs={6} md={4}>
-                    <h2 className="text-start">We provide all <br/>Dental Services</h2>
-                </Col>
-                <Col xs={12} md={8}>
-                    <p className="text-start">We offer you a wide range of dental services Our team of dentists is highly qualified and experienced. They are well versed with the latest technique of treatment and are able to treat patients with any sort of dental issue.</p>
-                </Col>
-            </Row>
-            </Container>
+            <div className="my-5">
+                <Container>
+                    <Row>
+                        <Col xs={6} md={4}>
+                            <h2 className="text-start">We provide all <br/>Dental Services</h2>
+                        </Col>
+                        <Col xs={12} md={8}>
+                            <p className="text-start">We offer you a wide range of dental services Our team of dentists is highly qualified and experienced. They are well versed with the latest technique of treatment and are able to treat patients with any sort of dental issue.</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+            <div>
+                <Row xs={2} md={3} className="g-4">
+                    {
+                        services.map(service => <Service
+                            key= {service.id}
+                            service={service}
+                        ></Service>)
+                    }
+                </Row>
+            </div>
         </div>
     );
 };
