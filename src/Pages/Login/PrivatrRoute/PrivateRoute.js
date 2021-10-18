5da3d64cbcc7e656d915ad5c72e8ccbf4ffa196a
mukitur <mukit.inway@gmail.com>
2021-10-18 20:00:08 +0600

    login reload problem solved

--- src/Pages/Login/PrivatrRoute/PrivateRoute.js
+++ src/Pages/Login/PrivatrRoute/PrivateRoute.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 import { Route, Redirect } from 'react-router-dom';
 import useAuth from '../../../hooks/useAuth';
 
 
 const PrivateRoute = ({ children, ...rest }) => {
     // const {children, ...rest} = props;
-    const { user } = useAuth();
+    const { user, isLoading } = useAuth();
+    if(isLoading){
+        return <Spinner animation="grow" variant="warning" />
+    }
     return (
         <Route
             {...rest}
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -7,10 +7,12 @@ initializeAuthentication();
 const useFirebase = () => {
     const [user, setUser] = useState({});
     const [error, setError] = useState('');
+    const [isLoading, setIsLoading] =useState(true);
     const auth = getAuth();
     const googleProvider = new GoogleAuthProvider();
     
     const signInWithGoogle =()=>{
+        setIsLoading(true);
         signInWithPopup(auth, googleProvider)
             .then(result =>{
                 console.log(result.user);
@@ -19,18 +21,22 @@ const useFirebase = () => {
             .catch(error => {
                 setError(error.message)
             })
+            .finally( () => setIsLoading(false))
     }
     const logout = () => {
+        setIsLoading(true);
         signOut(auth)
             .then(() => {
                 setUser({});
             })
+            .finally( () => setIsLoading(false))
     }
     useEffect(() => {
         onAuthStateChanged(auth, user => {
             if (user) {
                 setUser(user);
             }
+            setIsLoading(false);
         })
     }, []);
 
@@ -39,7 +45,8 @@ const useFirebase = () => {
             user,
             error,
             signInWithGoogle,
-            logout
+            logout,
+            isLoading
         }
     );
 };
