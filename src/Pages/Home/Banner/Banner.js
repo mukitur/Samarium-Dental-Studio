8bab90a4961945bccab7ae2ee7fd20029f2ae46c
mukitur <mukit.inway@gmail.com>
2021-10-19 11:16:04 +0600

    banner

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 import bgImage from '../../../images/banner-1.jpg';
 
@@ -9,8 +10,10 @@ const Banner = () => {
            <Card className="bg-dark text-dark">
             <Card.Img src= {bgImage} alt="Card image" />
             <Card.ImgOverlay>
-              <Card.Title><h1 style={{fontSize: '30px', color: '#2e7fc2'}} className="mt-4">WE <span className="text-primary" >CARE</span> FOR  <br/> YOUR SMILE</h1></Card.Title>
-              <Card.Text><Button variant="outline-primary">Appoinment</Button></Card.Text>
+              <Card.Title><h1 style={{fontSize: '30px'}} className="mt-4">WE <span className="text-primary" >CARE</span> FOR  <br/> YOUR SMILE</h1></Card.Title>
+              <Link to = "/appoinments">
+                        <Button variant="primary" size="lg"> Get Appointment </Button>
+                        </Link>
             </Card.ImgOverlay>
           </Card>
         </div>
