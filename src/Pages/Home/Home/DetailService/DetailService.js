9f8e4741ccaa11c7c9fa03c23f792f1762f08833
mukitur <mukit.inway@gmail.com>
2021-10-19 16:42:08 +0600

    updates

--- src/Pages/Home/Home/DetailService/DetailService.js
+++ src/Pages/Home/Home/DetailService/DetailService.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
-import { useParams } from 'react-router';
+import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 const DetailService = () => {
